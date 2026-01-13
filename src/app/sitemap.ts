98d115a3d9c604ab8801f54fc8a1338346a0d9dc
rafaelgaravello <shokopoka@gmail.com>
2026-01-13 11:24:38 -0300

    sitemap adjusted

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,9 +15,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
         lastModified: new Date(),
       },
     {
-      url: "https://syntesys.com.br/contact",
+      url: "https://syntesys.com.br/pt/contact",
       lastModified: new Date(),
     },
+    {
+        url: "https://syntesys.com.br/en/contact",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://syntesys.com.br/en/about",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://syntesys.com.br/pt/about",
+        lastModified: new Date(),
+      },
     {
         url: "https://syntesys.com.br/pt",
         lastModified: new Date(),
