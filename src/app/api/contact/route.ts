a178d3cd9ef973ed64faae436a6643643cb72ac3
rafaelgaravello <shokopoka@gmail.com>
2026-01-13 19:50:29 -0300

    env variables adjustment

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -151,6 +151,10 @@ const isPT = safeLocale === "pt"
     })
     console.log("RAW LOCALE:", locale)
     console.log("SAFE LOCALE:", safeLocale)
+    console.log("SMTP_HOST:", process.env.SMTP_HOST)
+console.log("SMTP_PORT:", process.env.SMTP_PORT)
+console.log("SMTP_USER:", process.env.SMTP_USER)
+console.log("SMTP_PASS:", process.env.SMTP_PASS ? "OK" : "MISSING")
     return NextResponse.json({ success: true })
   } catch (error) {
     console.error(error)
